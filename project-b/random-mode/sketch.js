36202515c5d9a99019d4b3ebcc0f817b46ae4dbe
Jae-Yang1227 <yy4127@nyu.edu>
2022-12-07 22:13:49 +0800

    ho

--- project-b/random-mode/sketch.js
+++ project-b/random-mode/sketch.js
@@ -1,24 +1,24 @@
 function preload() {
   aSound = loadSound(resource / A3.MP3)
   sSound = loadSound(resource / B3.MP3)
-  dSound = loadSound(resource / C4.MP3)
-  fSound = loadSound(resource / D4.MP3)
-  gSound = loadSound(resource / E4.MP3)
-  hSound = loadSound(resource / F4.MP3)
-  jSound = loadSound(resource / G4.MP3)
-  kSound = loadSound(resource / A4.MP3)
-  lSound = loadSound(resource / B4.MP3)
-  llSound = loadSound(resource / C5.MP3)
-  wSound = loadSound(resource / A33.MP3)
-  rSound = loadSound(resource / C33.MP3)
-  tSound = loadSound(resource / D33.MP3)
-  uSound = loadSound(resource / F33.MP3)
-  iSound = loadSound(resource / G33.MP3)
-  oSound = loadSound(resource / A44.MP3)
-  ooSound = loadSound(resource / C44.MP3)
+  // dSound = loadSound(resource / C4.MP3)
+  // fSound = loadSound(resource / D4.MP3)
+  // gSound = loadSound(resource / E4.MP3)
+  // hSound = loadSound(resource / F4.MP3)
+  // jSound = loadSound(resource / G4.MP3)
+  // kSound = loadSound(resource / A4.MP3)
+  // lSound = loadSound(resource / B4.MP3)
+  // llSound = loadSound(resource / C5.MP3)
+  // wSound = loadSound(resource / A33.MP3)
+  // rSound = loadSound(resource / C33.MP3)
+  // tSound = loadSound(resource / D33.MP3)
+  // uSound = loadSound(resource / F33.MP3)
+  // iSound = loadSound(resource / G33.MP3)
+  // oSound = loadSound(resource / A44.MP3)
+  // ooSound = loadSound(resource / C44.MP3)
 }
-let whiteSound = [aSound, sSound, dSound, fSound, gSound, hSound, jSound, kSound, lSound, llSound]
-let blackSound = [wSound, rSound, tSound, uSound, iSound, oSound, ooSound]
+// let whiteSound = [aSound, sSound, dSound, fSound, gSound, hSound, jSound, kSound, lSound, llSound]
+// let blackSound = [wSound, rSound, tSound, uSound, iSound, oSound, ooSound]
 let trans;
 let xPos;
 let spdX;
@@ -81,13 +81,15 @@ function draw() {
     for (let i = 0; i < whiteKeys.length; i++) {
       if (whiteKeys[i] == key) {
         whiteLights[i].display();
-        whiteSound[i].play();
+        // whiteSound[i].play();
+        aSound.play()
       }
     }
     for (let i = 0; i < blackKeys.length; i++) {
       if (blackKeys[i] == key) {
         blackLights[i].display();
-        blackSound[i].play();
+        // blackSound[i].play();
+        sSound.play()
       }
     }
   }
